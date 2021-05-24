2e29dcddca5dee070c4b6b46bee6ca4c4d7ce25d
Andrea Maric <andreamaric94@gmail.com>
2021-05-24 16:58:02 +0200

    changes

--- index.php
+++ index.php
@@ -6,19 +6,23 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">  <title>Cookie Bookie</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-  <header class="p-3 mb-3 border-bottom">
+  <header class="p-3 mb-3 shadow">
     <div class="container">
       <div class="d-flex align-items-center">
-        <h2 class="me-auto">Cookie Bookie</h2>
-        <button type="button" name="add_button" id="add_button" class="btn btn-success btn-xs" data-bs-toggle="modal" data-bs-target="#recipeModal">Add New Recipe</button>
+        <h2 class="me-auto mt-2">Cookie Bookie</h2>
+        <button type="button" name="add_button" id="add_button" class="btn btn-outline-success btn-sm" data-bs-toggle="modal" data-bs-target="#recipeModal">Add New Recipe</button>
       </div>
     </div>
   </header>
   <section>
     <div class="container">
-      <img src="header_img.jpg" class="img-fluid rounded" alt="food">
+      <form>
+        <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+      </form>
+      <img src="style/img/header_img.jpg" class="img-fluid rounded" alt="food">
       <div class="row d-flex justify-content-center mt-2 mt-md-4"></div>
     </div>
   </section>
@@ -30,14 +34,14 @@
           <h5 class="modal-title">Create Recipe</h5>
         </div>
         <form id="createRecipe" name="createRecipe" role="form">
-          <div class="modal-body">
+          <div class="modal-body d-grid gap-2">
             <div class="form-group">
                 <label>Name</label>
                 <input id="name" type="text" name="name" class="form-control">
             </div>
             <div class="form-group">
                 <label>Ingredients</label>
-                <input id="ingredients" type="text" name="ingredients" class="form-control" placeholder="">
+                <input id="ingredients" type="text" name="ingredients" class="form-control">
             </div>
             <div class="form-group">
                 <label>Time</label>
@@ -50,8 +54,8 @@
           </div>
           <div class="modal-footer">
             <input type="hidden" name="id" id="id">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <input type="submit" id="button_action" class="btn btn-info" value="Create">
+            <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Close</button>
+            <input type="submit" id="button_action" class="btn btn-outline-success" value="Create">
           </div>
         </form>
       </div>
@@ -74,12 +78,13 @@
         var output = '';
         $.each(result, function(key, val){
           output += '' +
-                  '<div class="col-7 col-sm-5 col-lg-3 rounded bg-light m-2 p-2 text-center">' +
+                  '<div class="col-7 col-sm-5 col-lg-3 rounded m-2 p-2 text-center shadow" id="recipe">' +
                     '<h4>' + val.recipe_name + '</h4>' +
                     '<p>' + val.ingredients + '</p>' +
                     '<p>Category: ' + val.category_name + '</p>' +
-                    '<button type="button" name="edit" class="btn btn-warning me-2 edit" id="' + val.id + '">Edit</button>' +
-                    '<button type="button" name="delete" class="btn btn-danger delete" id="' + val.id + '">Delete</button>' +
+                    '<p>Time: ' + val.time + '</p>' +
+                    '<button type="button" name="edit" class="btn btn-outline-warning me-2 btn-sm edit" id="' + val.id + '">Edit</button>' +
+                    '<button type="button" name="delete" class="btn btn-outline-danger btn-sm delete" id="' + val.id + '">Delete</button>' +
                   '</div>';
         });
         $(".row").html(output);
@@ -126,7 +131,7 @@
       //Create or update recipes
       $("#createRecipe").submit(function(event){
         event.preventDefault();
-        $.post("api/recipe/insert.php", $("form").serialize());
+        $.post("api/recipe/insert.php", $("#createRecipe").serialize());
         if($("#button_action").val() == 'Update'){
           alert("Recipe updated");
         } else {
